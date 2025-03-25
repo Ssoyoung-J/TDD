a87aa4f37ae4f82bfdaaab718b09c8f956594da8
soy5360 <soy5360@gmail.com>
2025-03-26 02:33:54 +0900

    test: test코드 작성

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,4 +1,23 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PointService {
+
+    /**
+     * 해당 Table을 활용하여 기능 작성
+     */
+    private UserPointTable userPointTable;
+
+    /**
+     *
+     * 특정 유저의 포인트를 충전하는 기능
+     * 유저의 포인트 충전 후 return될 값이 필요 없음
+     */
+    public UserPoint chargeUserPoint(long id, long amount) {
+        return userPointTable.insertOrUpdate(id, amount);
+    }
+
 }
--- src/test/java/io/hhplus/tdd/PointChargeTest.java
+++ src/test/java/io/hhplus/tdd/PointChargeTest.java
@@ -1,20 +1,64 @@
 package io.hhplus.tdd;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.hhplus.tdd.point.PointController;
+import io.hhplus.tdd.point.PointService;
 import io.hhplus.tdd.point.UserPoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static java.time.LocalTime.now;
+import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * 단위 테스트 작성
+ * */
+@ExtendWith(MockitoExtension.class)
 public class PointChargeTest {
 
-    /* API Test를 위한 Mock 객체 생성 필요*/
-    long currentTime = System.currentTimeMillis();
-    UserPoint userPoint = Mockito.mock(new UserPoint(1, 10, currentTime));
-//    @Test
-//    public void chargeApiTest(long id, long amount) {
-//        Mockito.when().thenReturn();
-//    }
+    @InjectMocks
+    private PointController pointController;
+
+    @Mock
+    private PointService pointService;
+
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(pointController).build();
+    }
+    
+
+    @DisplayName("포인트 충전 성공")
+    @Test
+    void pointChargeTest() throws Exception {
+        // given
+        long userId = 1L;
+        long chargeAmount = 10L;
+        UserPoint expectedUserPoint = new UserPoint(userId, chargeAmount, System.currentTimeMillis());
+        Mockito.when(pointService.chargeUserPoint(userId, chargeAmount)).thenReturn(expectedUserPoint);
+        UserPoint userPoint = pointService.chargeUserPoint(userId, chargeAmount);
+        System.out.println(userPoint.point());
+
+       assertThat(userPoint).isNotNull().returns(chargeAmount, UserPoint::point);
+
+    }
+
 
 }
